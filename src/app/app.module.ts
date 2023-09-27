d3ba43349e6011fbd429696f57c8ae91d9dfbbae
Donia <donia@aouali.net>
2023-09-27 23:46:26 +0200

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
+import { CocktailService } from './cocktail.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CocktailListComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CocktailService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
